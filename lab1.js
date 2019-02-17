86e534b5492494a27d49ee63c98ba9becf6fead3
Morrison18 <35890626+Morrison18@users.noreply.github.com>
2019-02-17 12:00:28 +0000

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,9 @@
+	function add(){
+		var result = 4 + 2;
+		alert("When the two numbers are added, the result is: " +result);
+	}
+	
+	function subtract(){
+		var result = 4 - 2;
+		alert("When the two numbers are subtracted, the result is: " +result);
+	}
